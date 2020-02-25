48b6cb71467534f5600d355aae13b20162c5c634
vijay18399 <vijayreddy18399@gmail.com>
2020-02-25 14:44:56 +0530

    added

--- index.js
+++ index.js
@@ -148,6 +148,24 @@ io.on("connection", socket => {
     });
    
   });
+  socket.on("gtagged", message => {
+    message.isTagged = true;
+    console.log(message.index);
+    index=message.index;
+    Message.updateOne({ _id : { $eq: message._id } }, message, (err, data) => {
+      if(data){
+        channel = 'gtagged';
+        message.index=index;
+        io.emit(channel, message);
+        console.log(message);
+      }
+      if (err) {
+        console.log(err);
+      }
+    });
+   
+  });
+  
   socket.on("gdeleted", message => {
     console.log(message);
     var Opted = message.Option;
@@ -225,23 +243,7 @@ io.on("connection", socket => {
   });
 });
 
-socket.on("gtagged", message => {
-  message.isTagged = true;
-  console.log(message.index);
-  index=message.index;
-  Message.updateOne({ _id : { $eq: message._id } }, message, (err, data) => {
-    if(data){
-      channel = 'gtagged';
-      message.index=index;
-      io.emit(channel, message);
-      console.log(message);
-    }
-    if (err) {
-      console.log(err);
-    }
-  });
- 
-});
+
 
 var port = process.env.PORT || 5000;
 
